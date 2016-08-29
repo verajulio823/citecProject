249c796854a657e9305e7f8906032b9fbf06f01b
julio vera <verajulio823@gmail.com>
2016-08-29 09:28:36 -0500

    changes paginator

--- CitieWeb/js/controllers/dashboard/indexController.js
+++
@@ -1,9 +0,0 @@
-angular
-  .module('appCitie')
-  .controller('indexController', ['$scope', '$location',
-    function($scope, $location) {
-
-		$scope.$parent.class_body = "hold-transition skin-blue sidebar-mini";
-		$scope.$parent.class_wrapper = "";
-
-  }]);
